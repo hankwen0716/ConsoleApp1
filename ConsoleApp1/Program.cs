b8dea8e112e25c72db463faf89d6c220be8850ac
user <user@DESKTOP-6JBEF5K>
2022-03-07 11:52:21 +0800

    餐費計算

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double meal_cost = 100;
+            int tip_percent = 15, tax = 8;
+            int total = tip_percent + tax + Convert.ToInt32(meal_cost);
+            Console.WriteLine("餐費一共是"+total);
+            Console.ReadLine();
         }
     }
 }
