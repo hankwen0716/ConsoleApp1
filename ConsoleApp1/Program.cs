7b2a541f9c79e64363412a037b60335944311d79
user <user@DESKTOP-6JBEF5K>
2022-03-14 10:45:47 +0800

    測試

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入數字:");
+            string input = Console.ReadLine();
+            int num = int.Parse(input);
+            if (num % 2 != 0)
+            {
+                Console.WriteLine("Weird");
+            }
+            else
+            {
+                if (num >= 2 && num <= 5)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+                else if (num >= 6 && num <= 20)
+                {
+                    Console.WriteLine("Weird");
+                }
+                else
+                {
+                    Console.WriteLine("Not Weird");
+                }
+            }
+            Console.WriteLine("按下任意按鈕結束程式");
+            Console.Read();
+        }
+    }
+}
